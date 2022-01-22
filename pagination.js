3ab839b98db20bf8c6162b150d12e6a1baaa5788
Josef Neureiter <josef.neu05@gmail.com>
2022-01-22 15:10:12 +0100

    Seite

---
+++ pagination.js
@@ -0,0 +1,11 @@
+
+
+document.write('<ul class="pagination justify-content-center">');
+  document.write('<li class="page-item"><a class="page-link" href="../index.html">Österreich</a></li>');
+  document.write('<li class="page-item"><a class="page-link" href="../Salzburg/salzburg.php">S</a></li>');
+  document.write('<li class="page-item"><a class="page-link" href="../Voralberg/voralberg.php">V</a></li>');
+  document.write('<li class="page-item"><a class="page-link" href="../Tirol/tirol.php">T</a></li>');
+  document.write('<li class="page-item"><a class="page-link" href="../Steiermark/steiermark.php">St</a></li>');
+  document.write('<li class="page-item"><a class="page-link" href="../Kaernten/kaernten.php">K</a></li>');
+  document.write('<li class="page-item"><a class="page-link" href="../Oberoe/oberoe.php">OÖ</a></li>');
+document.write('</ul>');
